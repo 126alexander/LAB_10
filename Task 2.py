9730bd2b99e0899d1cc350487f1df9aa58475fa8
aleksandr <alekc2019258@mail.ru>
2021-11-18 00:50:54 +0300

    test

---
+++ Task 2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+A = set(input("Введите первую строку: "))
+B = set(input("Введите вторую строку: "))
+
+print( f"A = {A}" )
+print( f"B = {B}" )
+
+print( f"A & B = {A & B}" )
+print( f"A - B = {A - B}" )
+print( f"A ^ B = {A ^ B}" )
